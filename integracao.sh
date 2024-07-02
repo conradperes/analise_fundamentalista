eafbf88fad39a05465fe1ed2c8a0d6c93ac4fc0f
CONRAD PERES <conrad.marques.peres@gmail.com>
2024-07-02 23:06:52 +0000

    Integracao de container stock com container influxdb na mesma rede

--- integracao.sh
+++ integracao.sh
@@ -1,4 +1,3 @@
-
 docker build -t stock .
 docker network create custom-net
 docker network connect custom-net influxdb
@@ -6,12 +5,12 @@ docker network connect custom-net stock
 docker inspect influxdb
 docker run --name stock --network custom-net \
   -e INFLUXDB_DB=cmp \
-  -e ticker=NVDA \
+  -e ticker=AMZN \
   -e INFLUXDB_USER=conrad \
   -e INFLUXDB_USER_PASSWORD=711724Cope \
   -e INFLUXDB_USER_READ=conrad \
   -e INFLUXDB_USER_WRITE=conrad \
   -e INFLUXDB_ORG=cmp \
-  -e INFLUXDB_TOKEN=y1F6vGWAN2ipu8iVCpSSPucvP6pmgPHs-saH69TuAEMieKEEmcjHfKA-_nrIwvC91SLPEEeJIDC7TW6Y41x8Pw== \
-  -e hostname=http://172.18.0.2:8086 \
-  stock-analysis
+  -e INFLUXDB_TOKEN=ac2HcJIpwxrOjfSUVt8NTGsBPkwHqj-_xEZnbtl5Z8BfqLQaZ7X1B9OAi52NmFZGjJ2b1RbY5iXT0iwtBEfnrA== \
+  -e hostname=http://172.19.0.3:8086 \
+  stock
