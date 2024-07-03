71ce99be9eeb2d3e61edd3226fd5743409658937
CONRAD PERES <conrad.marques.peres@gmail.com>
2024-07-03 00:09:05 +0000

    Classe python pegando dataset pandas e convertendo para Dataframe Spark e salvando no Influxdb via RDD com prticoes e foreach lambda e depois fechando a sessao spark no finally

--- spark/python-run.sh
+++ spark/python-run.sh
@@ -1,1 +1,11 @@
-python3 SaveWithSpark.py BTC-USD S9m9Ut6hy-N18yBX6Oo6df02FPYqZhxDYeye-JWnnJrTac4slbJipEcz0QXQ338PMjLcczu93rKDLFzwybGyzg==
+docker run --name save-spark-image --network custom-net \
+  -e INFLUXDB_DB=cmp \
+  -e ticker=FJPNX \
+  -e INFLUXDB_USER=conrad \
+  -e INFLUXDB_USER_PASSWORD=711724Cope \
+  -e INFLUXDB_USER_READ=conrad \
+  -e INFLUXDB_USER_WRITE=conrad \
+  -e INFLUXDB_ORG=cmp \
+  -e INFLUXDB_TOKEN=DMpI14RcKKEpcI-iG8tZUa7vi1BYy68IThwSQhNsdBfvqGpd2NndldP538pnaHME0_q1eKks2GKhFMn6KfB6xQ== \
+  -e hostname=http://172.18.0.2:8086 \
+    save-spark-image
